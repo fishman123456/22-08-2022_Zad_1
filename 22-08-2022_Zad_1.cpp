f51fece2dde141855163f400bebb6acb539fddf0
fishman123456 <den9152@yandex.ru>
2022-08-23 23:10:34 +0700

    23-08-2022  - вторник  делал дома, толком недоделал
    При переполнении стека нужно увеличивать его размер.

--- 22-08-2022_Zad_1.cpp
+++ 22-08-2022_Zad_1.cpp
@@ -10,13 +10,13 @@ using namespace std;
 class Stack
 {
 	//Нижняя и верхняя границы стека
-	int EMPTY {2};
-	int	FULL {5};
+	int EMPTY = 0;
+	int  FULL = 4;
 
 	//Массив для хранения данных
-	int st[FULL + 1];
+	int* st = new int[FULL];
 	//создаём динамический массив
-	int* mass = new int [0];
+	int* mass = new int[0];
 	//индекс определяющий вершину стека
 	int top;
 
@@ -26,7 +26,7 @@ public:
 	Stack();
 
 	//Добавление элемента
-	void Push(int c);
+	int Push(int c);
 
 	//Извлечение элемента
 	char Pop();
@@ -78,17 +78,28 @@ int Stack::GetCount()
 	return top + 1;
 }
 
-void Stack::Push(int c)
+int Stack::Push(int c)
 {
 	//здесь надо делать работу++++++++++++++++++++++++++++++++++++++++++++++++
 	//Если в стеке есть место, то увеличиваем указатель
 	//на вершину стека и вставляем новый элемент // убрал восклицательный знак
-
+	int uk{ 0 };
 	if (!IsFull())
 	{
 		st[++top] = c;
 	}
+
 	st[top + 1];
+	int* st1 = new int[FULL+1];
+	for (int i = 0; i < FULL; i++)
+	{
+		int ukaz_st = st[i];
+		st1[i] = ukaz_st;
+		int uk = i;
+	}
+	return 0;
+	st1[uk + 1] = 555;
+	return *st1;
 }
 
 
@@ -103,23 +114,26 @@ char Stack::Pop()
 }
 
 
-void main()
+int main()
 {
+	
 	srand(time(0));
 
 	Stack ST;
 	int c;
+	
 	//пока стек не заполнится // убрал восклицательный знак
 	while (!ST.IsFull()) {
 		c = rand() % 4 + 2;
 		ST.Push(c);
 	}
-
-	ST.Push(10);
-
+	
+	ST.Push(56);
 	//пока стек не освободится
 	while (c = ST.Pop()) {
 		cout << c << " ";
+		//cout << st1 << endl;
 	}
 	cout << "\n\n";
+	cout << __cplusplus;
 }
